63d26e31262245ae281c449b6850035592ead696
Dharmendra Patel <132144447+Dharmendra4455@users.noreply.github.com>
2025-03-25 19:51:20 -0700

    Add files via upload

---
+++ src/components/other/Newtask.jsx
@@ -0,0 +1,60 @@
+import { useContext } from "react";
+import Completed from "./Completed";
+import { SetlocalStorage } from "../../utils/localStorage";
+function Newtask(prop){
+   
+   const empdata=localStorage.getItem('employee')
+   const authdata=JSON.parse(empdata)
+   
+   const completetaskhandler=()=>{
+       const searchedemp=authdata.find((e)=>e.id==prop.data.id); //search emp in authemp data
+       console.log(prop.data.tasks.task[5])
+       const searchedtask=searchedemp.tasks.task.find((e)=>e.task_title==prop.task.task_title && e.task_date==prop.task.task_date && e.task_description==prop.task.task_description);
+       
+       searchedtask.completed=true;
+       searchedtask.new_task=false;
+       searchedemp.tasks.values.completed+=1;
+       if(searchedemp.tasks.values.new_task)
+       searchedemp.tasks.values.new_task-=1;
+         //console.log(searchedemp,searchedtask)
+       //  console.log(authdata)
+       //console.log(authdata)
+      localStorage.setItem('employee',JSON.stringify(authdata))
+       prop.fun()
+      
+    }
+    const failedtaskhandler=()=>{
+      const searchedemp=authdata.find((e)=>e.id==prop.data.id); //search emp in authemp data
+     
+      const searchedtask=searchedemp.tasks.task.find((e)=>e.task_title==prop.task.task_title && e.task_date==prop.task.task_date && e.task_description==prop.task.task_description);
+      
+      searchedtask.failed=true;
+      searchedtask.new_task=false;
+      searchedemp.tasks.values.failed+=1;
+      if(searchedemp.tasks.values.new_task)
+      searchedemp.tasks.values.new_task-=1;
+        //console.log(searchedemp,searchedtask)
+      //  console.log(authdata)
+      //console.log(authdata)
+     localStorage.setItem('employee',JSON.stringify(authdata))
+      prop.fun()
+     
+   }
+    return(<>
+    <div id="newtasklist" className="w-72 h-64  bg-gray-700 mt-3 ml-5 flex-nowrap  flex-shrink-0  rounded-md overflow-hidden ">
+         <div className="flex justify-between ">
+            <div className=" bg-red-700 h-fit w-fit ml-2 pl-3 pr-3 pt-1 pb-1 mt-2 rounded text-slate-50 ">High</div>
+            <div className=" pt-2 font-bold text-white pr-2">{prop.task.task_date}</div>
+         </div>
+         <h2 className="ml-5 mt-5 mr-5 text-lg text-white font-bold">{prop.task.task_title}</h2>
+         <div className="ml-5 text-sm pr-2 pt-2 h-20">{prop.task.task_description}</div>
+         <div className="flex justify-around">
+         </div>
+         <div className="flex pt-4" >
+            <button  onClick={completetaskhandler} className="bg-green-400  ml-6 from-neutral-950 font-bold rounded">Mark as Completed</button>
+            <button  onClick={failedtaskhandler} className="bg-red-500 ml-6 from-neutral-950 mr-6 font-bold rounded">Mark as Failed</button>
+         </div>
+      </div>
+    </>)
+} 
+ export default Newtask;
