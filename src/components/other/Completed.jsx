aa1d9a0234e87d20631e322423058a5fdcbaf79a
Dharmendra Patel <132144447+Dharmendra4455@users.noreply.github.com>
2025-03-27 16:19:08 +0530

    Add files via upload

---
+++ src/components/other/Completed.jsx
@@ -0,0 +1,12 @@
+function Completed(){
+    return(<>
+<div className=" w-72 h-64  bg-green-400 mt-3 ml-5 flex-nowrap  flex-shrink-0 rounded-md overflow-hidden ">     
+            <div className=" pt-2 text-white pr-2 font-bold text-right">11 Mar 2025</div>
+         <h2 className="ml-5 mt-5 mr-5 text-lg text-white font-bold">Make a youtube Video</h2>
+         <div className="ml-5 text-sm pr-2 pt-2 h-24">Lorem  totam consequuntur non, minima aut?
+         </div>
+           <div  className="bg-green-500 text-white pl-24 w-72 text-xl from-neutral-950 mr-6 font-bold  pt-2 pb-2">Completed</div>
+      </div>
+    </>)
+}
+export default Completed;
